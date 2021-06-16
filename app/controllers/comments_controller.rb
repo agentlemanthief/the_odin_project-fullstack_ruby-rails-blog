434a48550545baf2cadbe90b823cc7e65853a9a1
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-06-16 20:03:06 +0100

    Updates for heroku deployment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
   http_basic_authenticate_with name: 'nicholas', password: 'secret', only: :destroy
 
+  http_basic
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
